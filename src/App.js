e50cdf9ccf4596a19698a31195cbb742f4aa8619
gabriela729 <99573989+gabriela729@users.noreply.github.com>
2022-04-19 19:06:32 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    
+    <div >
+      <br></br>
+    <br></br>
+    <br></br>
+    <h1>NOMBRE DE LA INSTITUCION</h1>
+    <br></br>
+    <br></br>
+    <br></br>
+    
+    <img src="https://static.vecteezy.com/system/resources/previews/000/610/399/non_2x/vector-relationship-people-logos-school-education-logo-design-concept.jpg"/>
+    <br></br>
+    <br></br>
+    
+    <div class="contenido">
+    
+      <p>Nuestro contacto: 55 55 55 32</p>
+      <p>Ubicación: rosas #452 col. ------- código postal:----- </p>
+      <p>Correo electrónico: institución@html </p>
+    
+  </div>
+  
+  <div class="menu">
+
+  <h1>Derechos reservados</h1>
+
+</div>
+    <br></br>
+       
+   
+      
+   
+    
+    
+    
+ </div>
+  );
+}
+
+export default App;
